cd9348e5372b126014dd8029505fb921a09cece6
Jeff Nyman <jeffnyman@gmail.com>
2021-03-30 07:04:06 -0500

    More XHR style tests.

--- cypress/e2e/network.requests.spec.js
+++ cypress/e2e/network.requests.spec.js
@@ -1,4 +1,6 @@
 describe("Network Requests", () => {
+    let message = "Unable to find comment"
+
     beforeEach(() => {
         cy.visit("https://example.cypress.io/commands/network-requests")
 
@@ -7,8 +9,38 @@ describe("Network Requests", () => {
     })
 
     it("GET request", () => {
-        cy.route("GET", "comments/*").as("getComment")
+        // cy.route("GET", "comments/*").as("getComment")
+        cy.route({
+            method: "GET",
+            url: "comments/*",
+            response: "Testing Response"
+        }).as("getComment")
+
         cy.get(".network-btn").click()
         cy.wait("@getComment").its("status").should("eq", 200)
+    })
+
+    it("POST request", () => {
+        cy.route("POST", "/comments").as("postComment")
+        cy.get(".network-post").click()
+        cy.wait("@postComment").should((xhr) => {
+            expect(xhr.requestBody).to.include("email")
+            expect(xhr.responseBody).to.have.property("name", "Using POST in cy.intercept()")
+            expect(xhr.requestHeaders).to.have.property("Content-Type")
+        })
+    })
+
+    it("PUT request", () => {
+        cy.route({
+            method: "PUT",
+            url: "comments/*",
+            status: 404,
+            response: { error: message },
+            delay: 500
+        }).as("putComment")
+
+        cy.get(".network-put").click()
+        cy.wait("@putComment")
+        cy.get(".network-put-comment").should("contain", message)
     })
 })
